6bb47d52a22bfc03914d85306ec5f1b342982449
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-06-13 12:23:37 +0900

    add file5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Program, Provider, web3 } from "@project-serum/anchor";
 import idl from "./idl.json";
 import kp from "./keypair.json";
 
-
 require("dotenv").config();
 
 // SystemProgramはSolanaランタイムへの参照です。
